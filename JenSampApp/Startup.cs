aa2157b4d940b2555d827451d0e8ed59f4a926dc
pjalapur <pjalapur@OPT-LAP-0235>
2018-06-15 04:21:18 +0530

    Add project files.

---
+++ JenSampApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenSampApp.Startup))]
+namespace JenSampApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
